f2e9bf7989d8bcb984bb6ad39be03d6d0635425b
YuyaHoshida <yhoshida@sonix.asia>
2023-12-05 10:15:32 +0900

    Initial Commit

---
+++ MVC_learning/MVC_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVC_learningApp.swift
+//  MVC_learning
+//
+//  Created by yhoshida on 2023/12/05.
+//
+
+import SwiftUI
+
+@main
+struct MVC_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
